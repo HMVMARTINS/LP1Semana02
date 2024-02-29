196bd58641e91c2d5004011693bb2e8115d7cb7d
HMVMARTINS <hugomvmartins90@gmail.com>
2024-02-29 00:17:05 +0000

    Adiciona programa SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SmoothOperator
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Insere número inteiro não-negativo: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input >= 0)
+                }
+                    byte n = (byte)input:
+
+                    Console.WriteLine("Resultado da operação de decremento de n: " + (--n));
+                    Console.WriteLine("Resultado da operação de incremento de n: " + (++n));
+                }
+                else
+                {
+                    Console.WriteLine("Número inválido. Por favor, insira um númeor interiro não-negativo.");
+                }
+            }
+          
+
