227044a4f2cb5620db65ffd4d41034220ec26e98
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-01 00:15:33 +0000

    Estende o programa SmoothOperator com operações adicionais

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -5,19 +5,35 @@ namespace SmoothOperator
         {
             static void Main(string[] args)
             {
-                Console.Write("Insere número inteiro não-negativo: ");
+                Console.Write("Insera número inteiro não-negativo: ");
                 int input = Convert.ToInt32(Console.ReadLine());
 
-                if (input >= 0)
+                if (input >= 0 && userInput <=255) 
+                // Verifica se o número está dentro do intervalo do tipo byte
                 }
-                    byte n = (byte)input:
+                    byte n = (byte)userinput:
 
-                    Console.WriteLine("Resultado da operação de decremento de n: " + (--n));
-                    Console.WriteLine("Resultado da operação de incremento de n: " + (++n));
+                    //Resultado da operação de decremento de n
+                    Console.WriteLine($"Resultado da operação de decremento de n: {--n}");
+                    
+                    //Resultado da operação de incremento de n
+                    Console.WriteLine($"Resultado da operação de incremento de n: {++n}");
+
+                    //Resultado da divisão por 2
+                    Console.WriteLine($"Resultado da divisão por 2: {n/2}");
+
+                    //Resultado do shift left por 3
+                    Console.WriteLine($"Resultado do shift left por 3: {n<<3}");
+
+                    //Resultado do Xor com 5
+                    Console.WriteLine($"Resultado do Xor com 5: {n^5}");
+
+                    //Verifica se é maior que 10
+                    Console.WriteLine($"É maior que 10? {n>10}");
                 }
                 else
                 {
-                    Console.WriteLine("Número inválido. Por favor, insira um númeor interiro não-negativo.");
+                    Console.WriteLine("Número inválido. Por favor, insira um número interiro não-negativo dentro do intervalo de um byte (0-255).");
                 }
             }
           
